4316f11dbfa51a1503b5984273f049d0d9347de9
pratibham-cuelogic <pratibha.mishra@cuelogic.co.in>
2015-10-21 13:18:05 +0530

    Resoloved issue of undefined

--- JS/Day2/Program2/method_class.js
+++ JS/Day2/Program2/method_class.js
@@ -1,40 +1,18 @@
 var Student = function(name, age, city){
-	this.name = name
-	this.age = age
-	this.city = city
+    this.name = name
+    this.age = age
+    this.city = city
 }
 
 var student1 = new Student('Pratibha', 26, 'Nashik')
 var student2 = new Student('Priya', 20, 'Nashik')
 
-function Student(){}
 Student.prototype.sayHello = function(){
-  	console.log("Hello, I'm " + this.name + ', I am '+ this.age + ' years old and I live in '+ this.city +' city.');
+    return "Hello, I'm " + this.name + ', I am '+ this.age + ' years old and I live in '+ this.city +' city.';
 }
 
 var student3 = new Student('Abhishek', 22, 'Nashik')
-student3.sayHello()
-
-
-/*
-var Student = function (name, age, city) {
-  this.name = name
-  this.age = age
-  this.city = city
-}
-
-var student1 = new Student('Pratibha', 26, 'Nashik')
-var student2 = new Student('Priya', 20, 'Nashik')
-
-Student.prototype = {
-  sayHello:function(){
-  console.log("Hello, I'm " + this.name + ", I am "+ this.age + " years old and I live in "+ this.city +" city.")
- }
-}
-
-/*Student.prototype.sayHello = function () {
-  console.log("Hello, I'm " + this.name + ", I am "+ this.age + " years old and I live in "+ this.city +" city.");
-}
-
-var student3 = new Student('Abhishek', 22, 'Nashik')
-student3.sayHello()*/
+console.log(student1)
+console.log(student2)
+console.log(student3)
+console.log(student3.sayHello())
