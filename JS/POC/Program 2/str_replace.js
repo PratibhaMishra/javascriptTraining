1df34cc8721b6126d088f113672c92c395fbb0c7
pratibham-cuelogic <pratibha.mishra@cuelogic.co.in>
2015-10-27 12:29:13 +0530

    Program1 committing

--- JS/POC/Program 2/str_replace.js
+++ JS/POC/Program 2/str_replace.js
@@ -1,5 +1,5 @@
 var str = 'My name is Pranay';
-var stringReplace = function( str1, str2 ) {
+String.prototype.stringReplace = function( str1, str2 ) {
 	return str.split(str1).join(str2);
 };
 
--- JS/POC/Program 3/push_pop_array.js
+++ JS/POC/Program 3/push_pop_array.js
@@ -1,13 +1,13 @@
 var myArray = [ 1, 2, 3, 4 ];
-Array.prototype.customPush = function( element ) {
-	myArray[ myArray.length ] = element;
-	return myArray;
+Array.prototype.customPush  = function( element ) {
+	this[this.length] = element;
+	return this;
 };
 
-Array.prototype.customPop = function(){
-	return myArray.slice( 0, -1 );
+Array.prototype.customPop  =  function() {
+	return this.slice(0,-1);
 };
 
-myArray.customPush( 15 );
-myArray.customPush( 17 );
+myArray.customPush(6);
+myArray.customPush(7);
 myArray.customPop();
